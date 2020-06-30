b789c6f28d878359776a4a63a724538703f46d7e
Edgar Marques <edgar.eu1@hotmail.com>
2020-06-30 16:02:09 +0100

    Create the create method for Log

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -2,4 +2,21 @@ class LogsController < ApplicationController
   def new
     @log = Log.new
   end
+
+  def create
+    @log = Log.new(log_params)
+    @log.user = current_user
+
+    if @log.save
+      redirect_to log_path(@log)
+    else
+      render 'logs/new'
+    end
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id)
+  end
 end
