8b21c7cde092d204b9163dd9e9caf6d4c2dd3171
Thomas Gibson <gibsonjthomas96@gmail.com>
2020-07-01 17:57:39 +0100

    forgot to change branch, but ive added the geocode section. when user inputs nex location, the coordinates are automatically saved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_145156) do
+ActiveRecord::Schema.define(version: 2020_07_01_163104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_07_01_145156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.string "address"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
