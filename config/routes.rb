8adafaa169338e48ab3bc2beeff65606873c92a9
Laure Olivier <laurelacombeolivier@gmail.com>
2020-07-01 15:27:48 +0100

    styled show fish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'users#show'
 
   resources :logs
-  resources :fish, only: [:index]
+  resources :fish, only: [:index, :show]
   resources :locations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
