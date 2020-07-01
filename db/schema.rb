164a19eb8cd033a13194c5167e03fa9ffe6ef103
Thomas Gibson <gibsonjthomas96@gmail.com>
2020-07-01 16:00:46 +0100

    working on the image  seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_145509) do
+ActiveRecord::Schema.define(version: 2020_07_01_145156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_145509) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -42,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_06_30_145509) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
