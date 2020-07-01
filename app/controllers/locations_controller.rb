299649c053bd20f3d9bf09a47253eec226ce9a99
Thomas Gibson <gibsonjthomas96@gmail.com>
2020-07-01 10:05:01 +0100

    madethe fishes into cards. im struggling  todeal with the  locations aspect  of this. hard to get idea across to  TA i feel. maybe my lack of clarity though

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,32 @@
 class LocationsController < ApplicationController
+   def index
+    @locations = Location.all
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+  end
+
+  def create
+    @location = Location.new
+
+  end
+
+
 end
+
+
+
+
+#index
+#show
+#create
+#delete
+#update
+#new (show form to create new location)
+#edit (show form to upadte)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :logs
+  resources :fish, only: [:index]
+  resources :locations
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
