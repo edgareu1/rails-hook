87b969ea21ad309232200c0165e55baacdbe003f
Laure Olivier <laurelacombeolivier@gmail.com>
2020-07-02 17:42:16 +0100

    autocomplete done

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.6'
 
+# Auto-complete 
+gem "algoliasearch-rails"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use postgresql as the database for Active Record
@@ -32,6 +35,8 @@ gem 'geocoder'
 
 gem 'cloudinary'
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 gem 'devise'
 gem 'jquery-rails'
 
@@ -40,7 +45,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
-  gem 'dotenv-rails'
+  # gem 'dotenv-rails'
 
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -28,10 +29,15 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { bottom_fish } from 'bottom_fish';
+import { autoComplete } from '../plug-in/fish_auto_complete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  bottom_fish();
+  const bottomFishContainer = document.getElementById("jsi-flying-fish-container");
+  if (bottomFishContainer) {
+    bottom_fish();
+  }
+  autoComplete();
 });
 
