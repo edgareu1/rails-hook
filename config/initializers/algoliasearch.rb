d80dd8e710cd9ec34e42234f7daa6e1e59277d9b
Julie Mariano <j.mariano@live.be>
2020-07-03 11:06:25 +0100

    put api id for algolia

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,6 +1,6 @@
 AlgoliaSearch.configuration = { 
-    application_id: ENV['APPLICATION_ID'],
-    api_key: ENV['API_KEY'],
+    application_id: ENV['CF29YC2AOJ'],
+    api_key: ENV['6e83e6552e36d059a6c7ee6255c49e7a'],
     connect_timeout: 2,
     receive_timeout: 30,
     send_timeout: 30,
