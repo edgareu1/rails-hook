ba80b5c6d2bb9f99937395b2c956810563d214d1
Edgar Marques <edgar.eu1@hotmail.com>
2020-07-03 14:27:46 +0100

    Change weight in Catches to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_191034) do
+ActiveRecord::Schema.define(version: 2020_07_03_132523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "catches", force: :cascade do |t|
-    t.float "weight"
+    t.integer "weight"
     t.bigint "fish_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -68,10 +68,8 @@ ActiveRecord::Schema.define(version: 2020_07_01_191034) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
- 
   end
 
   add_foreign_key "catches", "fish"
