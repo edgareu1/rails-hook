a707d76797caef637afe811784b52e4a1cd68251
Edgar Marques <edgar.eu1@hotmail.com>
2020-07-06 13:14:21 +0100

    Add the air_pressure column to logs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_115455) do
+ActiveRecord::Schema.define(version: 2020_07_06_121224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_07_05_115455) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "tag_id"
+    t.integer "air_pressure"
     t.index ["location_id"], name: "index_logs_on_location_id"
     t.index ["user_id"], name: "index_logs_on_user_id"
   end
