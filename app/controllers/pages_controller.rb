db20eba46721f8e594ef36448b0dc0f28d839bf4
Edgar Marques <edgar.eu1@hotmail.com>
2020-07-08 01:39:39 +0100

    Normalize the logs average catch taking into account it's weather

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,7 @@ class PagesController < ApplicationController
   end
 
   def ranking
-    @top_ranking_hash = Ranking.new(current_user).rank
-    @top_ranking_arr = @top_ranking_hash.delete_if { |k, v| v[:classification].zero? }.map { |k, v| [k, v[:classification]] }.sort_by { |k, v| -v }[0..4]
-    raise
+    @all_location_stat_hash = Ranking.new(current_user).rank
+    @top_5_locations = @all_location_stat_hash.delete_if { |k, v| v[:classification].zero? || (v[:num_predicted_per_hour] * 5).round.zero? || (v[:gr_predicted_per_hour] * 5).round.zero? }.map { |k, v| [k, v[:classification]] }.sort_by { |k, v| -v }[0..4]
   end
 end
--- lib/ranking.rb
+++ lib/ranking.rb
@@ -4,72 +4,73 @@ class Ranking
   end
 
   def rank
-    arr_locations = []
+    arr_location_logs = []
 
     @user.location_ids.each do |location_id|
-      arr_location_logs = @user.logs.to_a.select { |log| log["location_id"] == location_id }
+      arr_logs = @user.logs.to_a.select { |log| log["location_id"] == location_id }
 
-      arr_locations << arr_location_logs unless arr_location_logs.empty?
+      arr_location_logs << arr_logs unless arr_logs.empty?
     end
 
-    # Until this point: arr_locations is an array with arrays of logs with the same location (no empty locations)
+    # Until this point: arr_location_logs is an array with arrays of logs with the same location (no empty locations)
 
-    all_ranking_hash = {}
-    top_ranking_hash = {}
+    all_location_hash = {}
+    all_location_stat_hash = {}
 
-    arr_locations.each do |location|
-      sum_location_classification = 0
+    arr_location_logs.each do |arr_logs|
       sum_location_hours = 0
-      sum_location_num_catches = 0
-      sum_location_weight_catches = 0
+      normalized_location_num_catches = 0
+      normalized_location_gr_catches = 0
 
-      location.each do |log|
-        sum_num_catches = 0
-        sum_gr_catches = 0
+      arr_logs.each do |log|
+        log_moon_phase_multiple = ((log.moon_phase - 0.5).abs * 2 * 0.8) + 1
+        log_air_pressure_multiple = air_pressure_indicator(log.air_pressure)
+        log_wind_speed_multiple = wind_speed_indicator(log.wind_speed)
+
+        log_multiple = log_moon_phase_multiple * log_air_pressure_multiple * log_wind_speed_multiple
 
         log.catches.each do |catch|
-          sum_num_catches += catch.quantity
-          sum_gr_catches += catch.weight
+          normalized_location_num_catches += catch.quantity / log_multiple
+          normalized_location_gr_catches += catch.weight / log_multiple
         end
 
-        sum_location_hours += (log.end_time - log.start_time).fdiv(60 * 60).round(1)
-        sum_location_num_catches += sum_num_catches
-        sum_location_weight_catches += sum_gr_catches
+        sum_location_hours += (log.end_time - log.start_time)
       end
 
-      all_ranking_hash[location.first.location_id] = {
-        number_hour: sum_location_num_catches / sum_location_hours,
-        weight_hour: sum_location_weight_catches / sum_location_hours,
+      sum_location_hours = sum_location_hours.fdiv(60 * 60).round(1)
+
+      all_location_hash[arr_logs.first.location_id] = {
+        num_per_hour: normalized_location_num_catches / sum_location_hours,
+        gr_per_hour: normalized_location_gr_catches / sum_location_hours,
       }
     end
 
-
-    top_ones = all_ranking_hash.each do |k, v|
+    all_location_hash.each do |k, v|
       weather_data = Rails.configuration.open_weather_api.current lon: Location.find(k).longitude , lat: Location.find(k).latitude
 
-      moon_phase_multiple = ( ( (moon_calculation(Time.now, Time.now + (5 * 60 * 60)) - 0.5).abs * 0.8 * 2) + 1 )
-      air_pressure_multiple = air_pressure_indicator(weather_data["main"]["pressure"])
-      wind_speed_multiple = wind_speed_indicator(weather_data["wind"]["speed"])
+      log_moon_phase_multiple = ((moon_calculation(Time.now, Time.now + (5 * 60 * 60)) - 0.5).abs * 2 * 0.8) + 1
+      log_air_pressure_multiple = air_pressure_indicator(weather_data["main"]["pressure"])
+      log_wind_speed_multiple = wind_speed_indicator(weather_data["wind"]["speed"])
 
-      multiple = moon_phase_multiple * air_pressure_multiple * wind_speed_multiple
+      log_multiple = log_moon_phase_multiple * log_air_pressure_multiple * log_wind_speed_multiple
 
-      num_fish_predicted = v[:number_hour] * multiple
-      weight_fish_predicted = v[:weight_hour] * multiple
+      num_predicted_per_hour = v[:num_per_hour] * log_multiple
+      gr_predicted_per_hour = v[:gr_per_hour] * log_multiple
 
-      top_ranking_hash[k] = {
-        number_hour: v[:number_hour],
-        weight_hour: v[:weight_hour],
-        moon_phase: moon_phase_multiple,
-        air_pressure_indicator: air_pressure_multiple,
-        wind_speed_indicator: wind_speed_multiple,
+      all_location_stat_hash[k] = {
+        num_per_hour: v[:num_per_hour],
+        gr_per_hour: v[:gr_per_hour],
+        moon_phase_multiple: log_moon_phase_multiple,
+        air_pressure_multiple: log_air_pressure_multiple,
+        wind_speed_multiple: log_wind_speed_multiple,
         weather_icon: weather_data["weather"][0]["icon"],
-        number_predicted: num_fish_predicted,
-        weight_predicted: weight_fish_predicted,
-        classification: (num_fish_predicted * 0.2) + ((weight_fish_predicted / 1000) * 0.8)
+        num_predicted_per_hour: num_predicted_per_hour,
+        gr_predicted_per_hour: gr_predicted_per_hour,
+        classification: (num_predicted_per_hour * 0.2) + ((gr_predicted_per_hour / 1000) * 0.8)
       }
     end
 
-    return top_ranking_hash
+    return all_location_stat_hash
   end
 
   def moon_calculation(start_time, end_time)
