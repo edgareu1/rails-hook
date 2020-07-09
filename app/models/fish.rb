4850b61f167e6934d2cc283ebd14cbe316ba0e51
Thomas Gibson <gibsonjthomas96@gmail.com>
2020-07-09 16:36:03 +0100

    added with master edgars help, the top 3 fish counts in tthe user page

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,7 +1,12 @@
 class Fish < ApplicationRecord
   include AlgoliaSearch
-  
+
   algoliasearch do
     attribute :common_name, :id
   end
+
+  def count_catches
+    Catch.all.to_a.select { |catch| catch.fish_id == self.id }.inject(0) { |sum, catch| sum + catch.quantity }
+  end
+
 end
