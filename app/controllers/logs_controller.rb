4a25fce07fb9d31fd2829569d48b1947fe470c3c
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-29 18:31:30 +0100

    Refactor and comment the Logs Controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,7 +3,6 @@ require 'will_paginate/array'
 class LogsController < ApplicationController
   before_action :set_log, only: [:show, :update, :destroy]
 
-
   def create
     @log = Log.new(log_params_new)
     @log.user = current_user
@@ -12,41 +11,34 @@ class LogsController < ApplicationController
     if @log.save
       redirect_to log_path(@log)
     else
-      @logs = current_user.logs.to_a.sort_by { |log| log["start_time"] }.reverse.paginate(page: params[:page], per_page: 5)
-      render :index
+      @logs = index_logs_initializer(current_user)
+      render "logs/index"
     end
   end
 
-
   def index
     @log = Log.new
-    @logs = current_user.logs.to_a.sort_by { |log| log["start_time"] }.reverse.paginate(page: params[:page], per_page: 5)
+    @logs = index_logs_initializer(current_user)
   end
-
 
   def show
     @catch = Catch.new
   end
 
-
   def update
-    logs_same_location = current_user.logs.to_a.select { |log| log.location == Location.find(params["log"]["location_id"]) }
-
-    if logs_same_location.empty?
-      @log.update(tag_id: 1)
-
-    elsif @log.location != Location.find(params["log"]["location_id"])
-      max_tag = logs_same_location.max_by { |element| element.tag_id }.tag_id
-      @log.update(tag_id: max_tag + 1)
-    end
+    previous_location_id = @log.location_id
 
     @log.update(log_params_edit)
-
     @log.update(moon_phase: moon_calculation(@log.start_time, @log.end_time))
+
+    unless previous_location_id == @log.location_id
+      # In order to not count this same tag_id in the calculation of the new tag_id
+      @log.update(tag_id: 0)
+      @log.update(tag_id: get_tag_id(@log))
+    end
 
     redirect_to log_path(@log)
   end
-
 
   def destroy
     @log.destroy
@@ -54,14 +46,32 @@ class LogsController < ApplicationController
     redirect_to logs_path
   end
 
-
   private
 
-  # A num based on the tag_id of the last log with the same location
+  def log_params_new
+    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id)
+  end
+
+  def log_params_edit
+    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id, :air_pressure, :wind_speed)
+  end
+
+  def set_log
+    @log = Log.find(params[:id])
+  end
+
+  # Gets the next tag_id for a Log of a certain location
   def get_tag_id(log)
     location_logs = log.location.logs
 
     return location_logs.empty? ? 1 : location_logs.map(&:tag_id).max + 1
+  end
+
+  # Gets the collection of Logs to display at the Logs#index Page
+  def index_logs_initializer(user)
+    return user.logs
+               .sort_by(&:start_time)
+               .reverse.paginate(page: params[:page], per_page: 5)
   end
 
   def moon_calculation(start_time, end_time)
@@ -77,20 +87,5 @@ class LogsController < ApplicationController
     else
       return (1 - moon_percentage) * 2
     end
-  end
-
-
-  def log_params_new
-    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id)
-  end
-
-
-  def log_params_edit
-    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :location_id, :air_pressure, :wind_speed)
-  end
-
-
-  def set_log
-    @log = Log.find(params[:id])
   end
 end
