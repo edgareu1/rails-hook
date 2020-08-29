f4791d01b4c71f58ecc2914cf4e53d6af297a35c
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-29 16:29:54 +0100

    Refactor the Locations Controller and Model

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,6 +5,6 @@ class Location < ApplicationRecord
   geocoded_by :name
   after_validation :geocode, if: :will_save_change_to_name?
 
-  validates :site, presence: true, uniqueness: true, :length => { :maximum => 20, :message => "Maximum of 20 characters"}
   validates :name, presence: true
+  validates :site, presence: true, uniqueness: true, length: { maximum: 20, message: "Maximum of 20 characters"}
 end
