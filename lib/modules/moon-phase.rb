66224ae9e0fd72ee3a32823e70c62b1bb64d567a
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-30 00:22:20 +0100

    Improve the get_moon_phase method with a more scientific aproach

--- lib/modules/moon-phase.rb
+++ lib/modules/moon-phase.rb
@@ -1,15 +1,27 @@
 module MoonPhase
-  def get_moon_phase(start_time)
-    new_moon_base = Time.new(2020, 06, 21, 6, 41, 00)
-    moon_cycle = 29.5 * 60 * 60 * 24
+  # Get the % of the moon that is active in a certain date
+  # Both this method and it's helper are based on the Git Repo of "ablwr" named "moon phases"
+  def get_moon_phase(date)
+    year = date.year
+    month = date.month
+    day = date.day
 
-    time_past_new_moon = (start_time - new_moon_base) % moon_cycle
-    moon_percentage = (time_past_new_moon / moon_cycle)
+    phase_days = (julian(year, month, day) - julian(2000, 1, 6)) % 29.530588853
 
-    if moon_percentage < 0.5
-      return moon_percentage * 2
+    if phase_days < 14.7652944265
+      return (phase_days % 14.7652944265) / 14.7652944265
     else
-      return (1 - moon_percentage) * 2
+      return 1 - ((phase_days % 14.7652944265) / 14.7652944265)
     end
+  end
+
+  # Convert a date in Julian
+  # Helper method for the calculation of the moon phase
+  def julian(year, month, day)
+    a = (14 - month) / 12
+    y = year + 4800 - a
+    m = (12 * a) - 3 + month
+
+    return day + (153 * m + 2) / 5 + (365 * y) + (y / 4) - (y / 100) + (y / 400) - 32045
   end
 end
