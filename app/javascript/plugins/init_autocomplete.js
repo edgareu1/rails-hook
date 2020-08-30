d5368a37a6904c0ed64fd9d8ceaa2583f92836a4
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-30 12:55:42 +0100

    Organize and refactor the JS components/plugins

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -2,6 +2,7 @@ import places from 'places.js';
 
 const initAutocomplete = () => {
   const addressInput = document.getElementById('location_name');
+
   if (addressInput) {
     places({ container: addressInput });
   }
