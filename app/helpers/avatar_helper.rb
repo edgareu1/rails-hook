ec7a6d746c4f37b9910520962c7c9da3a8c7c4e7
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-06 21:37:41 +0100

    Update the Seed file to create a User with an Avatar

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,8 +1,8 @@
 module AvatarHelper
   def avatar_large(user)
     if user.avatar.attached?
-      cl_image_tag(current_user.avatar.key, transformation: [
-        { width: 800, height: 800, gravity: 'face', radius: 'max', crop: 'fill' }
+      cl_image_tag(user.avatar.key, transformation: [
+        { width: 240, height: 240, gravity: 'face', radius: 'max', crop: 'fill' }
       ], class: "avatar-large")
     else
       image_tag('default-user-avatar.png', class: "avatar-large")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 User.destroy_all
 Log.destroy_all
@@ -60,6 +54,11 @@ puts "Created #{Fish.count} fish"
 
 
 laure = User.new(username: "Thomas", email: "thomas@gmail.com", password: "123456")
+
+# Attach a random avatar image
+file_avatar = URI.open('https://source.unsplash.com/random/400x400')
+laure.avatar.attach(io: file_avatar, filename: 'nes.png', content_type: 'image/png')
+
 laure.save
 
 puts "Created #{User.count} user"
