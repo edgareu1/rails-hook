6ba017ed879dfd30e97b02cc08247d2c8c2c767f
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-07 17:45:32 +0100

    Put the rounded_weight method of the Fish Model inside the ApplicationHelper Module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Returns a string with it's weight in the correct quantifications (gr or kg)
+  def rounded_weight(weight)
+    if weight == 0
+      return "NA"
+    elsif weight < 1000
+      return "#{weight} gr"
+    else
+      weight_in_kg = weight.fdiv(1000).tap { |n| break n.to_i == n ? n.to_i : n }
+      return "#{weight_in_kg} kg"
+    end
+  end
 end
