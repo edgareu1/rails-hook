99faab891d0ddb9df115969406aa4c96bed68651
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-07 17:58:34 +0100

    Put the AvatarHelper Module inside the ApplicationHelper Module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  # Returns a string with it's weight in the correct quantifications (gr or kg)
+  # Return a string with it's weight in the correct quantification (gr or kg)
   def rounded_weight(weight)
     if weight == 0
       return "NA"
@@ -8,6 +8,17 @@ module ApplicationHelper
     else
       weight_in_kg = weight.fdiv(1000).tap { |n| break n.to_i == n ? n.to_i : n }
       return "#{weight_in_kg} kg"
+    end
+  end
+
+  # Display the user avatar or, if this isn't present, the standard avatar
+  def large_avatar(user)
+    if user.avatar.attached?
+      cl_image_tag(user.avatar.key, transformation: [
+        { width: 240, height: 240, gravity: 'face', radius: 'max', crop: 'fill' }
+      ], class: "avatar-large")
+    else
+      image_tag('default-user-avatar.png', class: "avatar-large")
     end
   end
 end
