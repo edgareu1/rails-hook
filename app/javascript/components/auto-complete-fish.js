b73941eb516616b875c809fe584664cb3a8a687a
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-09 22:04:29 +0100

    Display the matched elements in the View

--- app/javascript/components/auto-complete-fish.js
+++ app/javascript/components/auto-complete-fish.js
@@ -3,10 +3,31 @@ function autoCompleteFish(searchField) {
     const { value } = event.target;                 // Search param
     const fish_names = gon.fish_names.split(', ');  // Array of Fish to search into
 
+    // Create the 'autocomplete-list' Div
+    let autoCompleteContainer = document.createElement("div");
+    autoCompleteContainer.setAttribute("id", "autocomplete-list");
+
+    // Insert the 'autocomplete-list' in the document
+    searchField.parentNode.insertBefore(autoCompleteContainer, searchField.nextSibling);
+
+    searchField.parentNode.style.position = 'relative';
+
+    // Iterate over the array of Fish
     for (let i = 0; i < fish_names.length; i++) {
-      // Iterate over the array of Fish and check if the element includes the search param
-      if (fish_names[i].toUpperCase().includes(value.toUpperCase())) {
-        console.log(fish_names[i]);
+      // Check if the element matches the search param
+      let wordIndex = fish_names[i].toUpperCase().indexOf(value.toUpperCase());
+
+      // If it matches then...
+      if (wordIndex >= 0) {
+        let fishElement = document.createElement("div");
+
+        // Make the matching letters bold
+        fishElement.innerHTML = fish_names[i].substr(0, wordIndex);
+        fishElement.innerHTML += "<strong>" + fish_names[i].substr(wordIndex, value.length) + "</strong>";
+        fishElement.innerHTML += fish_names[i].substr(wordIndex + value.length);
+
+        // Insert the matched element in the autoComplete list
+        autoCompleteContainer.appendChild(fishElement);
       }
     }
   });
