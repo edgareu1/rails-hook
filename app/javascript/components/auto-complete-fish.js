08eb97977d7d6ac6701563bc62a28930d4914bc2
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-10 11:00:46 +0100

    Add style to the autocomplete list

--- app/javascript/components/auto-complete-fish.js
+++ app/javascript/components/auto-complete-fish.js
@@ -1,6 +1,6 @@
 function autoCompleteFish(searchField) {
   searchField.addEventListener('input', (event) => {
-    const { value } = event.target;                 // Search param
+    const param = event.target.value.trim();        // Search param striped of trailing whitespaces
     const fish_names = gon.fish_names.split(', ');  // Array of Fish to search into
 
     // Get or create 'autocomplete-list' Div
@@ -21,10 +21,12 @@ function autoCompleteFish(searchField) {
       emptyList(autoCompleteContainer); // Make sure the List is empty
     }
 
+    if (param === '') return; // If the param is empty then let the List also be empty
+
     // Iterate over the array of Fish
     for (let i = 0; i < fish_names.length; i++) {
       // Check if the element matches the search param
-      let wordIndex = fish_names[i].toUpperCase().indexOf(value.toUpperCase());
+      let wordIndex = fish_names[i].toUpperCase().indexOf(param.toUpperCase());
 
       // If it matches then...
       if (wordIndex >= 0) {
@@ -32,8 +34,8 @@ function autoCompleteFish(searchField) {
 
         // Make the matching letters bold
         fishElement.innerHTML = fish_names[i].substr(0, wordIndex);
-        fishElement.innerHTML += "<strong>" + fish_names[i].substr(wordIndex, value.length) + "</strong>";
-        fishElement.innerHTML += fish_names[i].substr(wordIndex + value.length);
+        fishElement.innerHTML += "<strong>" + fish_names[i].substr(wordIndex, param.length) + "</strong>";
+        fishElement.innerHTML += fish_names[i].substr(wordIndex + param.length);
 
         // Insert the matched element in the autoComplete list
         autoCompleteContainer.appendChild(fishElement);
