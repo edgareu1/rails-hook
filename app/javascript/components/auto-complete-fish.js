a7f9914a65dc33abe45baea022c5031c1ab35f6f
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-11 19:01:06 +0100

    Refactor and comment the JS autoCompleteFish function

--- app/javascript/components/auto-complete-fish.js
+++ app/javascript/components/auto-complete-fish.js
@@ -1,34 +1,37 @@
+// Create an autocomplete list for the Fish search while refreshing the Fish#index page automatically (using AJAX)
+// based on the search parameter
 function autoCompleteFish(searchField) {
   // Define two variables to follow both the items indexes and the currently selected item index
-  let indexCounter, activeItemIndex;
+  let indexCounter, selectedItemIndex;
 
   // Each time the user writes on the 'searchField', then...
   searchField.addEventListener('input', (event) => {
-    // Use the gem 'gon' in order to call the Fish Controller variable 'fish_names'
+    // Use the gem 'gon' to call the Fish Controller variable 'fish_names'
     const fishNames = gon.fish_names.split(', ');   // Array of Fish to search into
     const param = event.target.value.trim();        // Search param striped of trailing whitespaces
 
-    indexCounter = -1;      // No item is yet created
-    activeItemIndex = -1;   // No item is selected
+    indexCounter = -1;      // The autocomplete list is empty
+    selectedItemIndex = -1;   // No item is selected
 
-    let listContainer = document.getElementById("autocomplete-list"); // Get the List of the matched items
+    let autoCompleteList = document.getElementById("autocomplete-list"); // Get the autocomplete list
 
-    // If the List does not exist, then create it
-    if (!listContainer) {
-      listContainer = document.createElement("div");
-      listContainer.setAttribute("id", "autocomplete-list");
+    // If the autocomplete list does not exist, then create it
+    if (!autoCompleteList) {
+      autoCompleteList = document.createElement("div");
+      autoCompleteList.setAttribute("id", "autocomplete-list");
 
-      searchField.parentNode.insertBefore(listContainer, searchField.nextSibling);
+      let searchFieldContainer = searchField.parentNode;
+      searchFieldContainer.appendChild(autoCompleteList);
 
-      // Make sure the position of the List is relative to the 'searchField'
-      searchField.parentNode.style.position = 'relative';
+      // Make sure the position of the autocomplete list is relative to the 'searchField'
+      searchFieldContainer.style.position = 'relative';
 
-    // If the List already exists, then empty it
+    // If the autocomplete list already exists, then empty it
     } else {
       emptyList();
     }
 
-    // If the param is empty, then do not populate the List and refresh the Fish#index Page
+    // If the param is empty, then just refresh the Fish#index Page
     if (param === '') {
       refreshPageSearch(param);
       return;
@@ -39,7 +42,7 @@ function autoCompleteFish(searchField) {
       // Check if the item matches the search param
       let wordIndex = fishNames[i].toUpperCase().indexOf(param.toUpperCase());
 
-      // If it matches, then add the Fish to the List
+      // If it matches, then add the Fish to the autocomplete list
       if (wordIndex >= 0) {
         // Make sure the maximum number of matches displayed is 5
         indexCounter++;
@@ -50,15 +53,15 @@ function autoCompleteFish(searchField) {
         // Save the index of the item in a data attribute
         fishElement.setAttribute('data-index', indexCounter);
 
-        // Make the matching letter are bold
+        // Make the matching letters bold
         fishElement.innerHTML = fishNames[i].substr(0, wordIndex);
         fishElement.innerHTML += "<strong>" + fishNames[i].substr(wordIndex, param.length) + "</strong>";
         fishElement.innerHTML += fishNames[i].substr(wordIndex + param.length);
 
-        // Insert the matched item in the List
-        listContainer.appendChild(fishElement);
+        // Insert the matched item in the autocomplete list
+        autoCompleteList.appendChild(fishElement);
 
-        // If the item is clicked upon, then the 'searchField' value becomes the that item's value
+        // If the item is clicked upon, then the 'searchField' is filled with that item's value
         fishElement.addEventListener('click', function(e) {
           searchField.value = fishNames[i];
 
@@ -66,11 +69,11 @@ function autoCompleteFish(searchField) {
           emptyList();
         });
 
-        // Each time the user hovers it's mouse over the item makes it the 'active' item
+        // Each time the user hovers it's mouse over the item makes it the 'selected' item
         fishElement.addEventListener("mouseover", function(e) {
-          activeItemIndex = fishElement.getAttribute('data-index');
-          removeActive();
-          addActive();
+          selectedItemIndex = fishElement.getAttribute('data-index');
+          removeSelected();
+          addSelected();
         });
       }
     }
@@ -80,41 +83,42 @@ function autoCompleteFish(searchField) {
 
   // Each time the user presses down a key on the 'searchField'...
   searchField.addEventListener("keydown", function(e) {
-    // Only advance if the List exists and has items
-    let listContainer = document.getElementById("autocomplete-list");
-    if ((!listContainer) || listContainer.childElementCount == 0) return;
+    // Only advance if the autocomplete list exists and has items
+    let autoCompleteList = document.getElementById("autocomplete-list");
+    if ((!autoCompleteList) || autoCompleteList.childElementCount == 0) return;
 
-    // If the arrow DOWN key is pressed, then the 'active' item becomes the one with +1 index value
+    // If the arrow DOWN key is pressed, then the 'selected' item becomes the one with +1 index value
     if (e.keyCode == 40) {
-      activeItemIndex++;
-      removeActive();
-      addActive();
+      selectedItemIndex++;
+      removeSelected();
+      addSelected();
 
-    // If the arrow UP key is pressed, then the 'active' item becomes the one with -1 index value
+    // If the arrow UP key is pressed, then the 'selected' item becomes the one with -1 index value
     } else if (e.keyCode == 38) {
-      activeItemIndex--;
-      removeActive();
-      addActive();
+      selectedItemIndex--;
+      removeSelected();
+      addSelected();
 
-    // If the ENTER key is pressed, then prevent the form from being submitted and simulate the click on the 'active' item
+    // If the ENTER key is pressed, then prevent the form from being submitted and simulate the click on the
+    // 'selected' item
     } else if (e.keyCode == 13) {
       e.preventDefault();
 
-      listContainer = listContainer.getElementsByTagName("div");
-      if (activeItemIndex > -1) listContainer[activeItemIndex].click();
+      autoCompleteList = autoCompleteList.getElementsByTagName("div");
+      if (selectedItemIndex > -1) autoCompleteList[selectedItemIndex].click();
     }
   });
 
-  // If the user clicks outside the 'searchField' or the 'List' empty the 'List'
+  // If the user clicks outside the 'searchField' or the autocomplete list, then empty the autocomplete list
   document.addEventListener("click", function(e) {
     if (e.target.hasAttribute('data-index') || e.target.id == 'search-input') return;
     emptyList();
   });
 
-  // Empty the 'List'
+  // Empty the autocomplete list
   function emptyList() {
-    let listContainer = document.getElementById("autocomplete-list");
-    if (listContainer) listContainer.innerHTML = '';
+    let autoCompleteList = document.getElementById("autocomplete-list");
+    if (autoCompleteList) autoCompleteList.innerHTML = '';
   }
 
   // Use jQuery to refresh the Fish#index Page based on that search param
@@ -122,24 +126,24 @@ function autoCompleteFish(searchField) {
     $.getScript(`/fish?query=${param}&commit=Search`);
   }
 
-  // Remove the 'active' classification from the previous 'active' item
-  function removeActive() {
-    let listContainer = document.getElementById("autocomplete-list")
+  // Remove the 'selected' classification from the previous 'selected' item
+  function removeSelected() {
+    let autoCompleteList = document.getElementById("autocomplete-list")
                                 .getElementsByTagName("div");
 
-    let activeElement = document.getElementsByClassName('autocomplete-active')[0];
-    if (activeElement) activeElement.classList.remove("autocomplete-active");
+    let selectedElement = document.getElementsByClassName('autocomplete-selected')[0];
+    if (selectedElement) selectedElement.classList.remove("autocomplete-selected");
   }
 
-  // Get the new 'active' item (which highlights the item)
-  function addActive() {
-    let listContainer = document.getElementById("autocomplete-list")
+  // Get the new 'selected' item (which highlights the item)
+  function addSelected() {
+    let autoCompleteList = document.getElementById("autocomplete-list")
                                 .getElementsByTagName("div");
 
-    if (activeItemIndex >= listContainer.length) activeItemIndex = 0;
-    if (activeItemIndex < 0) activeItemIndex = (listContainer.length - 1);
+    if (selectedItemIndex >= autoCompleteList.length) selectedItemIndex = 0;
+    if (selectedItemIndex < 0) selectedItemIndex = (autoCompleteList.length - 1);
 
-    listContainer[activeItemIndex].classList.add("autocomplete-active");
+    autoCompleteList[selectedItemIndex].classList.add("autocomplete-selected");
   }
 }
 
