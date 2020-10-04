04f592650a8413f47669e37d32534611775800f4
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-04 22:12:33 +0100

    Comment the JS updateCanvasDimensions function

--- app/javascript/components/update-canvas-dimensions.js
+++ app/javascript/components/update-canvas-dimensions.js
@@ -1,11 +1,15 @@
-// Update the Canvas (Fish Tank) size to fill the whole window of the user's device
+// Function that updates the Canvas dimensions to fill the whole window of the Users device
+// The parameter is the canvas to which the dimensions is to be updated
 function updateCanvasDimensions(canvas) {
+  // Get the height and width of the Users device
   let windowWidth = window.innerWidth;
   let windowHeight = window.innerHeight;
 
+  // Set the canvas dimensions to be equal to the ones of the Users device
   canvas.width = windowWidth;
   canvas.height = windowHeight;
 
+  // Set the canvas container dimensions to equal to the ones of the Users device
   canvas.style.width = `${windowWidth}px`;
   canvas.style.height = `${windowHeight}px`;
 };
