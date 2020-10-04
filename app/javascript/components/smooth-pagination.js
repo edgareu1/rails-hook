2743dcabef597d38c0e7d7eaa3d2eb7cd1a1b919
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-04 22:07:05 +0100

    Comment the JS smoothPagination function

--- app/javascript/components/smooth-pagination.js
+++ app/javascript/components/smooth-pagination.js
@@ -1,20 +1,21 @@
-// Function that makes the pagination anchors scroll up to the beginning of the page
+// Function that make the pagination anchors scroll to the beginning of the page in a smooth manner
 function smoothPagination() {
   const anchors = document.getElementsByClassName("page-link"); // Get the anchors of the pagination
 
-  // For each of the anchors do...
+  // For each of the anchors...
   for (let anchor of anchors) {
     const anchorRef = anchor.href;
 
-    // Make sure the anchor has a page destination
+    // If the anchor has a page destination, then...
     if (typeof anchorRef !== "undefined") {
       anchor.setAttribute("data-remote", "true"); // Ajaxify the anchor
 
       // Upon clicking on the anchor...
       anchor.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent the anchor's default click behavior
+        event.preventDefault(); // Prevent the anchors default click behavior
 
         // Use jQuery to perform a scrolling animation to the top of the page
+        // 250 is the time (in milliseconds) the animation is going to take
         $(".body-container").animate( {
           scrollTop: 0
         }, 250);
