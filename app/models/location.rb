a14f86d784b25d7bfd94a3070924526e8ad09120
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-11 21:09:58 +0100

    Update the Locations spot column validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,12 +6,12 @@ class Location < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_name?
 
   validates :name, presence: true
-  validates :spot, presence: true, length: { maximum: 20, message: "Maximum of 20 characters"}
+  validates :spot, presence: true, length: { maximum: 16, message: "Maximum of 16 characters"}
   validates_uniqueness_of :spot, case_sensitive: false, scope: :user_id
 
   # Makes sure the name displayed isn't to long
   def name_to_display
-    name.match(/^[^,]*/)[0].truncate(20)
+    name.match(/^[^,]*/)[0][0..15].strip
   end
 
   # Gets the current Weather Data of the Location
