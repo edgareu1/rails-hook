1ee62b3734ecf7001500660877e68f9a9b349d66
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 21:50:14 +0100

    Create the API/Logs#destroy method

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LogsController < Api::V1::BaseController
-  before_action :set_user_and_location, only: [ :index, :show, :create, :update ]
+  before_action :set_user_and_location, only: [ :index, :show, :create, :update, :destroy ]
+  before_action :set_log,               only: [ :update, :destroy ]
 
   def index
     @logs = @location.logs
@@ -23,8 +24,6 @@ class Api::V1::LogsController < Api::V1::BaseController
   end
 
   def update
-    @log = @location.logs.find(params[:id])
-
     if @log.update(log_params)
       render :show
     else
@@ -32,15 +31,23 @@ class Api::V1::LogsController < Api::V1::BaseController
     end
   end
 
+  def destroy
+    @log.destroy
+  end
+
   private
+
+  def log_params
+    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :air_pressure, :wind_speed, :moon_phase, :weather_description, :weather_icon)
+  end
 
   def set_user_and_location
     @user = User.find(params[:user_id])
     @location = @user.locations.find(params[:location_id])
   end
 
-  def log_params
-    params.require(:log).permit(:start_time, :end_time, :rating, :observation, :air_pressure, :wind_speed, :moon_phase, :weather_description, :weather_icon)
+  def set_log
+    @log = @location.logs.find(params[:id])
   end
 
   def render_error
