82a40f7e368511f60387969147789e4c0d791e66
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 18:33:52 +0100

    Create the API/Locations#create method

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::LocationsController < Api::V1::BaseController
-  before_action :set_user, only: [ :index, :show ]
+  before_action :set_user, only: [ :index, :show, :create ]
 
   def index
     @locations = @user.locations
@@ -9,9 +9,28 @@ class Api::V1::LocationsController < Api::V1::BaseController
     @location = @user.locations.find(params[:id])
   end
 
+  def create
+    @location = @user.locations.new(location_params)
+
+    if @location.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
+
+  def location_params
+    params.require(:location).permit(:name, :spot)
+  end
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def render_error
+    render json: { errors: @location.errors.full_messages },
+           status: :unprocessable_entity
   end
 end
