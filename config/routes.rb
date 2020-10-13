5258fda78de8542ab69081139565005c2d50d992
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 14:29:33 +0100

    Create the API/Users#show route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :fish, only: [ :index ]
+
+      resources :users, only: [ :show ]
     end
   end
 end
