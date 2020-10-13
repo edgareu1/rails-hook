a79f97088cdb80c5cbbba091d0611538a316ecee
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 15:32:14 +0100

    Create the API/Logs#index and show methods

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::LogsController < Api::V1::BaseController
+  before_action :set_user_and_location, only: [ :index, :show ]
+
+  def index
+    @logs = @location.logs
+  end
+
+  def show
+    @log = @location.logs.find(params[:id])
+  end
+
+  private
+
+  def set_user_and_location
+    user = User.find(params[:user_id])
+    @location = user.locations.find(params[:location_id])
+  end
 end
