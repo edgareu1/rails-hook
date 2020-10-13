c9fb2c0e73b6de1a1b1c48a8f0adc9843fcf68fd
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 15:10:32 +0100

    Create the API/Locations#index and show methods

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::LocationsController < Api::V1::BaseController
+  before_action :set_user, only: [ :index, :show ]
+
+  def index
+    @locations = @user.locations
+  end
+
+  def show
+    @location = @user.locations.find(params[:id])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
