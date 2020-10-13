61e5f67909456957f81e9105bf5e549e11799f10
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-13 21:27:45 +0100

    Create the API/Logs#update route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
       resources :users, only: [ :show ] do
         resources :locations, only: [ :index, :show, :create, :update, :destroy ] do
-          resources :logs, only: [ :index, :show, :create ]
+          resources :logs, only: [ :index, :show, :create, :update ]
         end
       end
     end
