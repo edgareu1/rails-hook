457747ea48324f57236a78baf91c82312531f037
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-15 18:16:11 +0100

    Create the Locations#show method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,6 @@
 class LocationsController < ApplicationController
   include ApplicationHelper
+  include MoonPhaseHelper
 
   def create
     @location = current_user.locations.new(location_params)
@@ -11,6 +12,27 @@ class LocationsController < ApplicationController
     @locations = current_user.locations
                              .order(logs_count: :desc)
                              .paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @location = Location.find_by(id: params[:id])
+
+    if @location.nil? || @location.user != current_user
+      flash[:alert] = "That Location was not found"
+      redirect_to locations_path
+
+    else
+      weather_data = @location.fetch_weather_data
+
+      @location_weather = {
+        icon: weather_data["weather"].first["icon"],
+        description: weather_data["weather"].first["description"],
+        air_pressure: weather_data["main"]["pressure"],
+        wind_speed: weather_data["wind"]["speed"],
+        temperature: weather_data["main"]["temp"],
+        moon_phase: (get_moon_phase(Time.now) * 100).round
+      }
+    end
   end
 
   private
