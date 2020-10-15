4ab2d3a47347e9e3c1d55edad6f9a527fe2eec83
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-15 18:54:43 +0100

    Add the country_code column to the Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_175134) do
+ActiveRecord::Schema.define(version: 2020_10_15_175357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_175134) do
     t.string "spot"
     t.integer "logs_count", default: 0
     t.string "country"
+    t.string "country_code"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
