0e1d7830fafc62f6f400c1462a2e901e4a96e3e4
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-15 18:55:58 +0100

    Add the city column to the Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_175357) do
+ActiveRecord::Schema.define(version: 2020_10_15_175519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_175357) do
     t.integer "logs_count", default: 0
     t.string "country"
     t.string "country_code"
+    t.string "city"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
