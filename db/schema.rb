0a0c244f5f83bc2aa48b593d249a2514fe7a1f6e
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-15 18:57:08 +0100

    Add the state column to the Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_175519) do
+ActiveRecord::Schema.define(version: 2020_10_15_175625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_175519) do
     t.string "country"
     t.string "country_code"
     t.string "city"
+    t.string "state"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
