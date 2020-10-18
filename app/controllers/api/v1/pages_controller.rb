d87f93d221bc1d112af9359637ec82ba09186c79
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-18 16:08:21 +0100

    Create the API/Pages#location_prediction method

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::PagesController < Api::V1::BaseController
+  include PredictionHelper
+
+  acts_as_token_authentication_handler_for User
+
+  before_action :check_user_authorization
+
+  def location_prediction
+    @location = Location.find(params[:location_id])
+
+    if @location.logs.size < 5
+      @prediction = { weather: @location.weather_data, prediction: nil }
+    else
+      @prediction = Ranking.new(current_user).prediction(@location)
+    end
+  end
 end
