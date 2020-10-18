2fe77c3ec07f3aadb9e42fde5d8710bf55fc3b40
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-19 00:38:57 +0100

    Improve the validations of the weather attributes of the Logs table

--- app/models/log.rb
+++ app/models/log.rb
@@ -6,9 +6,11 @@ class Log < ApplicationRecord
   has_many :fish, through: :catches
 
   validates :location, presence: true
-  validates :temperature,  numericality: { greater_than_or_equal_to: 0 }
-  validates :air_pressure, numericality: { greater_than_or_equal_to: 0 }
-  validates :wind_speed,   numericality: { greater_than_or_equal_to: 0 }
+
+  # The weather attributes intervals take into account their respective lowest/highest records measured on Earth
+  validates :temperature,  numericality: { greater_than: -100, less_than: 100 },          on: :update
+  validates :air_pressure, numericality: { greater_than: 880, less_than: 1080 },          on: :update
+  validates :wind_speed,   numericality: { greater_than_or_equal_to: 0, less_than: 140 }, on: :update
 
   validate :log_duration
 
@@ -58,19 +60,19 @@ class Log < ApplicationRecord
   # Method that adds validations to the Log duration
   def log_duration
     if start_time.nil?
-      errors.add(:end_time, "Log has to have a start time")
+      errors.add(:start_time, "must exist")
 
     elsif end_time.nil?
-      errors.add(:end_time, "Log has to have a end time")
+      errors.add(:end_time, "must exist")
 
     elsif start_time >= end_time
-      errors.add(:end_time, "Log cannot end before it begins")
+      errors.add(:duration, "must be positive")
 
     elsif start_time + (9 * 60) + 1 >= end_time
-      errors.add(:end_time, "Log cannot be less than 10min long")
+      errors.add(:duration, "cannot be less than 10min")
 
     elsif start_time + (60 * 60 * 24) + 1 <= end_time
-      errors.add(:end_time, "Log cannot be longer than 24h")
+      errors.add(:duration, "cannot be longer than 24h")
     end
   end
 end
