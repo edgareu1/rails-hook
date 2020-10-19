e5b90820c84a832236e75ed4f31f6d06668205d3
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-19 17:02:22 +0100

    Add numericality inclusion validations to the Catch attributes

--- app/models/catch.rb
+++ app/models/catch.rb
@@ -2,6 +2,8 @@ class Catch < ApplicationRecord
   belongs_to :fish
   belongs_to :log
 
-  validates :quantity, presence: true
-  validates :weight, presence: true
+  validates :quantity, presence: true,
+                       numericality: { greater_than: 0, less_than: 100 }
+  validates :weight,   presence: true,
+                       numericality: { greater_than: 0 }
 end
