c5e25c67fdb40178dcbbf97ab0250eb103a20d2e
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-20 20:01:22 +0100

    Add the catches_weight column to the Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_183938) do
+ActiveRecord::Schema.define(version: 2020_10_20_185946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_183938) do
     t.string "city"
     t.string "state"
     t.integer "catches_count", default: 0
+    t.integer "catches_weight", default: 0
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
