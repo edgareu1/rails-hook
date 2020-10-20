1c76cde25dc4adc613efa951a3900ac10d9007ee
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-21 00:33:01 +0100

    Refactor the API/Pages location info display

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -6,12 +6,12 @@ class Api::V1::PagesController < Api::V1::BaseController
   before_action :check_user_authorization
 
   def location_prediction
-    @location = current_user.locations.find(params[:location_id])
+    location = current_user.locations.find(params[:location_id])
 
-    if @location.logs.size < 5
-      @prediction = { weather: @location.weather_data, prediction: nil }
+    if location.logs.size < 5
+      @prediction = { weather: location.weather_data, prediction: nil }
     else
-      @prediction = Ranking.new(current_user).prediction(@location)
+      @prediction = Ranking.new(current_user).prediction(location)
     end
   end
 
