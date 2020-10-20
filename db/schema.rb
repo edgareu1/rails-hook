275375813796f43eb6601d2e26169cddf99b52ed
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-20 19:59:31 +0100

    Add the catches_count column to the Locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_175056) do
+ActiveRecord::Schema.define(version: 2020_10_20_183938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_175056) do
     t.string "country_code"
     t.string "city"
     t.string "state"
+    t.integer "catches_count", default: 0
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
