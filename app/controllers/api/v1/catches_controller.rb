6f2f575ae0477b75777dac10246429fc0fd9c282
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-21 02:13:58 +0100

    Create the API/Catches#index controller method

--- app/controllers/api/v1/catches_controller.rb
+++ app/controllers/api/v1/catches_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::CatchesController < Api::V1::BaseController
+  before_action :check_user_authorization
+  before_action :set_log, only: [ :index ]
+
+  def index
+    @catches = @log.catches.sort
+  end
+
+  private
+
+  def set_log
+    @log = @user.logs.find(params[:log_id])
+  end
 end
