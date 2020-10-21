fb34d66e82ec5287f5070fb75105811b3ac79c31
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-21 02:18:56 +0100

    Create the API/Catches#create controller method

--- app/controllers/api/v1/catches_controller.rb
+++ app/controllers/api/v1/catches_controller.rb
@@ -1,14 +1,33 @@
 class Api::V1::CatchesController < Api::V1::BaseController
   before_action :check_user_authorization
-  before_action :set_log, only: [ :index ]
+  before_action :set_log, only: [ :index, :create ]
 
   def index
     @catches = @log.catches.sort
   end
 
+  def create
+    @catch = @log.catches.new(catch_params)
+
+    if @catch.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
+
+  def catch_params
+    params.require(:catch).permit(:fish_id, :quantity, :weight)
+  end
 
   def set_log
     @log = @user.logs.find(params[:log_id])
+  end
+
+  def render_error
+    render json: { errors: @catch.errors.full_messages },
+           status: :unprocessable_entity
   end
 end
