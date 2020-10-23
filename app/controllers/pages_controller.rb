e70db1c8461b089cf892eb0f86f644fec44a111e
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-23 20:59:45 +0100

    Create the Pages#location_prediction controller method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,14 @@ class PagesController < ApplicationController
   def prediction
     @prediction = Ranking.new(current_user).top_ranking_locations(3)
   end
+
+  def location_prediction
+    location = current_user.locations.find(params[:location_id])
+
+    if location.logs.size < 5
+      @prediction =  nil
+    else
+      @prediction = Ranking.new(current_user).prediction(location)
+    end
+  end
 end
