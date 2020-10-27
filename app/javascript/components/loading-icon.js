02f43595e1517b5329a8268bfb8c4286acae78d1
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-27 17:59:49 +0000

    Comment the JS components

--- app/javascript/components/loading-icon.js
+++ app/javascript/components/loading-icon.js
@@ -1,4 +1,6 @@
 // Function that displays a loading icon while the page loads
+// Arguments:
+//   loadingAnchor: Element where the loading icon will appear
 function loadingIcon(loadingAnchor) {
   loadingAnchor.addEventListener('click', () => {
     loadingAnchor.innerHTML = "<i class=\"fas fa-spinner fa-spin\"></i>";
--- app/javascript/components/update-canvas-dimensions.js
+++ app/javascript/components/update-canvas-dimensions.js
@@ -1,17 +1,18 @@
-// Function that updates the Canvas dimensions to fill the whole window of the Users device
-// The parameter is the canvas to which the dimensions is to be updated
+// Function that updates the Canvas dimensions to fill the whole window of the User device
+// Arguments:
+//   canvas: Canvas element that will have its dimensions updated
 function updateCanvasDimensions(canvas) {
-  // Get the height and width of the Users device
+  // Get the height and width of the User device
   let windowWidth = window.innerWidth;
   let windowHeight = window.innerHeight;
 
-  // Set the canvas dimensions to be equal to the ones of the Users device
+  // Set the canvas dimensions to be equal to the ones of the User device
   canvas.width = windowWidth;
   canvas.height = windowHeight;
 
-  // Set the canvas container dimensions to equal to the ones of the Users device
+  // Set the canvas container dimensions to equal to the ones of the User device
   canvas.style.width = `${windowWidth}px`;
   canvas.style.height = `${windowHeight}px`;
-};
+}
 
 export { updateCanvasDimensions };
