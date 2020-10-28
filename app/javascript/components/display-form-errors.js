a3fe942038354572699b7abf246042e7366433be
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-28 17:33:41 +0000

    Refactor the JS displayFormErrors function

--- app/javascript/components/display-form-errors.js
+++ app/javascript/components/display-form-errors.js
@@ -1,9 +1,9 @@
 // Function that displays an error message in a form
 // Arguments:
-//   errorSelector: Selector of the errors container; Eg: 'log_new_start_time_error', 'catch_new_error'
-//   errorMessage:  Error message to append to the form
+//   errorSelector: Selector of the errors container; Eg: 'log_new_time_errors', 'catch_new_errors'
+//   errorMessage:  Error message to display in the form
 function displayFormErrors(errorSelector, errorMessage) {
-  let errorsContainer = document.querySelector(`.${errorSelector}`);
+  let errorsContainer = document.getElementById(errorSelector);
   errorsContainer.innerHTML = errorMessage;
 }
 
