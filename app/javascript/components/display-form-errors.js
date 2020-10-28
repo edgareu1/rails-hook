9e3936394999bdd7f5a09ab7c616949a5dc8f672
Edgar Marques <edgar.eu1@hotmail.com>
2020-10-28 02:49:09 +0000

    Refactor the JS displayFormErrors function

--- app/javascript/components/display-form-errors.js
+++ app/javascript/components/display-form-errors.js
@@ -1,25 +1,10 @@
 // Function that displays an error message in a form
 // Arguments:
-//   errorSelector: Selector of the specific input error container; E.g.: 'log_new_start_time', 'catch_quantity'
+//   errorSelector: Selector of the errors container; Eg: 'log_new_start_time_error', 'catch_new_error'
 //   errorMessage:  Error message to append to the form
 function displayFormErrors(errorSelector, errorMessage) {
-  let inputField = document.getElementById(errorSelector); // Get the input field
-
-  // If the error message is empty, then mark the input field as valid
-  if (errorMessage == "") {
-    inputField.classList.remove("is-invalid");
-    inputField.classList.add("is-valid");
-
-  // Otherwise...
-  } else {
-    // Mark the input field as invalid
-    inputField.classList.remove("is-valid");
-    inputField.classList.add("is-invalid");
-
-    // Add the error message to its container
-    let errorsContainer = document.querySelector(`.${errorSelector}_error`);
-    errorsContainer.innerHTML = errorMessage;
-  }
+  let errorsContainer = document.querySelector(`.${errorSelector}`);
+  errorsContainer.innerHTML = errorMessage;
 }
 
 export { displayFormErrors };
